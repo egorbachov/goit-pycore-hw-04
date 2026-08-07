5ba1431717f8d693bb4d2d111cf7f36fe1396261
egorbachov <e.gorbachov@gmail.com>
2026-08-07 21:07:51 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,25 +1,12 @@
 def total_salary(path):
   try:
     with open(path, "r", encoding="utf-8") as file:
-      salaries = []
-      for line in file:
-        line = line.strip()
-        if not line:
-          continue
-        name, salary = line.split(",")
-        salaries.append(int(salary))
-    if not salaries:
-      return (0, 0)
-    total = sum(salaries)
-    average = total / len(salaries)
-    return (total, average)
+      lines = file.readlines()
   except FileNotFoundError:
-    print(f"Файл '{path}' не знайдено.")
-    return (0, 0)
-  except ValueError:
-    print("Помилка у форматі даних файлу.")
-    return (0, 0)
-
-if __name__ == "__main__":
-  total, average = total_salary("salary_file.txt")
-  print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+    return 0, 0
+  total = 0
+  for line in lines:
+    name, salary = line.split(",")
+    total += int(salary)
+  average = total / len(lines)
+  return total, average
