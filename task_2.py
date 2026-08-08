d483968c6fcc21f85e9717605e8b26066f0635fa
egorbachov <e.gorbachov@gmail.com>
2026-08-08 10:46:25 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,14 +1,21 @@
 def get_cats_info(path):
-  try:
-    with open(path, "r", encoding="utf-8") as file:
-      lines = file.readlines()
-  except FileNotFoundError:
-    return []
-  cats = []
-  for line in lines:
-    cat_id, name, age = line.strip().split(",")
-    cats.append({"id": cat_id, "name": name, "age": age})
-  return cats
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            lines = file.readlines()
+    except FileNotFoundError:
+        return []
+    cats = []
+    for line in lines:
+        line = line.strip()
+        if not line:
+            continue
+        try:
+            cat_id, name, age = line.split(",")
+        except ValueError:
+            continue
+        cats.append({"id": cat_id, "name": name, "age": age})
+    return cats
+
 
 cats_info = get_cats_info("cats_file.txt")
 print(cats_info)
